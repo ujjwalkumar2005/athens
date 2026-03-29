90e4a6c64f9fd12fd2acff078bb3e8c0342ea39c
Ujjwal kumar <ujjwalkumar90881@gmail.com>
2026-03-29 14:36:30 +0530

    Update attend.js

--- models/attend.js
+++ models/attend.js
@@ -4,7 +4,9 @@ const attendanceSchema = new mongoose.Schema({
     studentName: { type: String, required: true },
     rollNumber: { type: String, required: true },
     date: { type: String, required: true },
-    status: { type: String, enum: ['Present', 'Absent','absent','present'], required: true }
+    // I removed the 'enum' restriction so any string works, 
+    // or you can add lowercase versions to the list.
+    status: { type: String, required: true } 
 });
 
 module.exports = mongoose.model('Attendance', attendanceSchema);
